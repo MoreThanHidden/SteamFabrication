f163815fd10d249ed396391904de2eda482e319b
MoreThanHidden (John816) <john816_@hotmail.com>
2016-11-20 01:09:02 +0800

    Getting it all ready!
    -Added whats needed from the forge mdk
    -Edited the build.gradle
    -Made some basic models and textures

---
+++ src/main/java/morethanhidden/steamfabrication/SteamFabrication.java
@@ -0,0 +1,17 @@
+package morethanhidden.steamfabrication;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = SteamFabrication.MODID, version = SteamFabrication.VERSION)
+public class SteamFabrication
+{
+    public static final String MODID = "steamfabrication";
+    public static final String VERSION = "0.1a";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+
+    }
+}
