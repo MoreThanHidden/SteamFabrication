9cb32a89cb54763a3869fed740fc46b7903afd11
MoreThanHidden (John816) <john816_@hotmail.com>
2016-12-09 21:26:53 +0800

    Fixed Copper Ore Issues

--- src/main/java/morethanhidden/steamfabrication/blocks/BlockBase.java
+++ src/main/java/morethanhidden/steamfabrication/blocks/BlockBase.java
@@ -3,6 +3,9 @@ package morethanhidden.steamfabrication.blocks;
 import morethanhidden.steamfabrication.SteamFabrication;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.ResourceLocation;
 
 public class BlockBase extends Block{
@@ -12,6 +15,24 @@ public class BlockBase extends Block{
 		setCreativeTab(SteamFabrication.tabsteamfabrication);
 		setUnlocalizedName(unlocalisedname);
 		setRegistryName(new ResourceLocation(SteamFabrication.MODID, getUnlocalizedName().substring(5)));
+		setHardness(3);
+	}
+
+
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state)
+	{
+		return EnumBlockRenderType.MODEL;
+	}
+
+	@Override
+	public BlockRenderLayer getBlockLayer() {
+		return  BlockRenderLayer.CUTOUT;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
 	}
 
 }
